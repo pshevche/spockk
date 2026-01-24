4651574f2b320131bcf7b894b88b3531de306d36
Pavlo Shevchenko <pavel.shevchenko.95@gmail.com>
2026-01-24 22:27:16 +0100

    Update IntelliJ version under test

--- spockk-intellij-plugin/build.gradle.kts
+++ spockk-intellij-plugin/build.gradle.kts
@@ -9,7 +9,7 @@ repositories { intellijPlatform { defaultRepositories() } }
 
 dependencies {
   intellijPlatform {
-    intellijIdea("2025.3.1")
+    intellijIdea("2025.3.2")
 
     bundledPlugin("org.jetbrains.kotlin")
     bundledPlugin("org.jetbrains.plugins.gradle")
