1ec56e90e3d788a03c4d97bf8c8bcbd1fe618394
Pavlo Shevchenko <pavel.shevchenko.95@gmail.com>
2026-07-05 21:42:39 +0200

    test(core): add basic tests for transformation of conditions

---
+++ spockk-specs/src/test/resources/samples/compilation/transformed/condition/BasicImplicitCondition.kt
@@ -0,0 +1,39 @@
+@org.spockframework.runtime.model.SpecMetadata(filename = "BasicImplicitCondition.kt", line = 1)
+class BasicImplicitCondition : spock.lang.Specification() {
+  @org.spockframework.runtime.model.FeatureMetadata(
+    ordinal = 0,
+    name = "some feature",
+    line = 2,
+    parameterNames = [],
+    blocks = [org.spockframework.runtime.model.BlockMetadata(
+      org.spockframework.runtime.model.BlockKind.EXPECT,
+      [""]
+    )]
+  )
+  fun `$spock_feature_0_0`() {
+    val `$spock_valueRecorder` : org.spockframework.runtime.ValueRecorder = org.spockframework.runtime.ValueRecorder()
+    val `$spock_errorCollector` : org.spockframework.runtime.ErrorRethrower =
+      org.spockframework.runtime.ErrorRethrower.INSTANCE
+    org.spockframework.runtime.SpockRuntime.callBlockEntered(this, 0)
+    try {
+      org.spockframework.runtime.SpockRuntime.verifyCondition(`$spock_errorCollector`,
+        `$spock_valueRecorder`.reset(),
+        "true",
+        4,
+        5,
+        null,
+        `$spock_valueRecorder`.record(`$spock_valueRecorder`.startRecordingValue(0), true))
+    }
+    catch (`$spock_condition_throwable` : Throwable) {
+      org.spockframework.runtime.SpockRuntime.conditionFailedWithException(`$spock_errorCollector`,
+        `$spock_valueRecorder`,
+        "true",
+        4,
+        5,
+        null,
+        `$spock_condition_throwable`)}
+    finally {
+    }
+    org.spockframework.runtime.SpockRuntime.callBlockExited(this, 0)
+  }
+}
