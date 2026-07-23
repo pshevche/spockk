a1d4b9dca06518da50beb2f05bbabd24cb2ac128
Pavlo Shevchenko <pavel.shevchenko.95@gmail.com>
2026-07-23 21:49:41 +0200

    feat: add SpockkRunLineMarkerContributor for gutter icons
    The Kotlin plugin's KotlinTestRunLineMarkerContributor uses
    KotlinPsiBasedTestFramework for test detection, which doesn't
    recognize Spockk specs. Without our own contributor, no gutter
    icons appear on Spockk spec classes or feature methods.
    
    - Add SpockkRunLineMarkerContributor extending RunLineMarkerContributor
    - register with language='kotlin' so it fires for Kotlin PSI elements
    - Check parent element: KtClassOrObject (spec) or KtNamedFunction (feature)
    - Verify nameIdentifier match to limit icon to the class/method name
    - Delegate click handling to ExecutorAction for standard run/debug flow

---
+++ spockk-intellij-plugin/src/main/kotlin/io/github/pshevche/spockk/intellij/SpockkRunLineMarkerContributor.kt
@@ -0,0 +1,44 @@
+/*
+ * Copyright 2026 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.github.pshevche.spockk.intellij
+
+import com.intellij.execution.lineMarker.ExecutorAction
+import com.intellij.execution.lineMarker.RunLineMarkerContributor
+import com.intellij.icons.AllIcons
+import com.intellij.psi.PsiElement
+import org.jetbrains.kotlin.psi.KtClassOrObject
+import org.jetbrains.kotlin.psi.KtNamedFunction
+import org.jetbrains.kotlin.psi.KtObjectDeclaration
+
+class SpockkRunLineMarkerContributor : RunLineMarkerContributor() {
+
+  override fun getInfo(element: PsiElement): Info? = calculateIcon(element)
+
+  override fun getSlowInfo(element: PsiElement): Info? = calculateIcon(element)
+
+  private fun calculateIcon(element: PsiElement): Info? {
+    val parent = element.parent
+    val actions = when {
+      parent is KtClassOrObject && parent !is KtObjectDeclaration &&
+        parent.nameIdentifier == element && parent.isSpockkSpec() ->
+        ExecutorAction.getActions(0)
+      parent is KtNamedFunction && parent.nameIdentifier == element &&
+        parent.isSpockkFeature() ->
+        ExecutorAction.getActions(1)
+      else -> return null
+    }
+    return Info(AllIcons.RunConfigurations.TestState.Run, actions)
+  }
+}
